e55530c2a5e57287325f3f5a96ea1480a11f7bf1
Luthfi Khusyasy <khusyasy@gmail.com>
2020-08-17 10:42:20 +0700

    fix static folder, add more to product card

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/images/products', express.static('public'));
+app.use(express.static('public'));
 
 var routes = require('./routes');
 routes(app);
