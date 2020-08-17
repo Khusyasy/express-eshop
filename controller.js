5a39dc6a2e6fed65ed7182c6c089a86cab4d4899
Luthfi Khusyasy <khusyasy@gmail.com>
2020-08-17 09:05:37 +0700

    init project

---
+++ controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var connection = require('./conn');
+
+exports.index = function(req, res) {
+    res.render('homepage');
+};
+
+exports.product = function(req, res) {
+    res.render('product/');
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require('./routes');
+routes(app);
+
+app.listen(3000);
